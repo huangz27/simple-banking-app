b3a677908a3fbebee189aeadfc34a68abc3d0fde
ZhiWei Huang <huangz27@hotmail.com>
2025-05-11 17:28:57 +0800

    change pgsql db parameter group to match engine version

--- terraform/main/rds.tf
+++ terraform/main/rds.tf
@@ -33,7 +33,7 @@ resource "aws_db_instance" "banking_db" {
   db_name                = var.db_name
   username               = var.db_username
   password               = random_password.db_password.result
-  parameter_group_name   = "default.postgres14"
+  parameter_group_name   = "default.postgres16"
   db_subnet_group_name   = aws_db_subnet_group.banking_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
   skip_final_snapshot    = true
